8b2e91c75b9dab3d5906fd1259172341d51a3ed0
Mario Delgado <mario.delgado@galaxy.com>
2025-04-01 23:54:25 +0100

    enable using a distributed setup

--- fpd/docker-entrypoint.sh
+++ fpd/docker-entrypoint.sh
@@ -13,7 +13,7 @@ fi
 echo "Updating config..."
 
 sed -i "s/EOTSManagerAddress = 127.0.0.1:12582/EOTSManagerAddress = eotsd:${EOTSD_PORT}/" /data/fpd/fpd.conf
-sed -i "s|RPCAddr = http://localhost:26657|RPCAddr = http://${RPC_HOST}:${CL_RPC_PORT}|" /data/fpd/fpd.conf
+sed -i "s|RPCAddr = http://localhost:26657|RPCAddr = ${BABYLOND_HOST}|" /data/fpd/fpd.conf
 sed -i "s|^ChainID = .*|ChainID = ${NETWORK}|" /data/fpd/fpd.conf
 sed -i '/^\[metrics\]/,/^\[/{ /^[^[]/ s/127\.0\.0\.1/0.0.0.0/g }' /data/fpd/fpd.conf
 sed -i 's/^\(\s*BatchSubmissionSize\s*=\s*\).*/\1 100/' /data/fpd/fpd.conf
