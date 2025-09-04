62ab267fc7fb7f8c515c45d8a9a0815c784e6244
Antonio Savage <antonio.savage@galaxy.com>
2025-09-04 15:42:10 -0400

    BCE-6965 New config values for Babylon finality hotfix

--- eotsd/docker-entrypoint.sh
+++ eotsd/docker-entrypoint.sh
@@ -11,6 +11,17 @@ fi
 echo "Initialization complete. Ready to start eotsd."
 
 sed -i '/^\[metrics\]/,/^\[/{ /^[^[]/ s/127\.0\.0\.1/0.0.0.0/g }' /data/eotsd/eotsd.conf
+# Add GRPCMaxContentLength if not present
+if ! grep -q '^GRPCMaxContentLength' /data/eotsd/eotsd.conf; then
+  cat <<EOF >> /data/eotsd/eotsd.conf
+[Application Options]
+; The maximum size of the gRPC message in bytes.
+GRPCMaxContentLength = 16777216
+EOF
+else
+  # Update existing GRPCMaxContentLength value to 16777216
+  sed -i 's/^\(\s*GRPCMaxContentLength\s*=\s*\).*/\1 16777216/' /data/eotsd/eotsd.conf
+fi
 
 # Word splitting is desired for the command line parameters
 # shellcheck disable=SC2086
--- fpd/docker-entrypoint.sh
+++ fpd/docker-entrypoint.sh
@@ -16,15 +16,35 @@ sed -i "s|^EOTSManagerAddress = .*|EOTSManagerAddress = ${EOTSD_HOST}|" /data/fp
 sed -i "s|^RPCAddr = .*|RPCAddr = ${BABYLOND_HOST}|" /data/fpd/fpd.conf
 sed -i "s|^ChainID = .*|ChainID = ${NETWORK}|" /data/fpd/fpd.conf
 sed -i '/^\[metrics\]/,/^\[/{ /^[^[]/ s/127\.0\.0\.1/0.0.0.0/g }' /data/fpd/fpd.conf
-sed -i 's/^\(\s*BatchSubmissionSize\s*=\s*\).*/\1 100/' /data/fpd/fpd.conf
+# sed -i 's/^\(\s*BatchSubmissionSize\s*=\s*\).*/\1 100/' /data/fpd/fpd.conf
+sed -i 's/^\(\s*BatchSubmissionSize\s*=\s*\).*/\1 250/' /data/fpd/fpd.conf
+sed -i 's/^\(\s*BufferSize\s*=\s*\).*/\1 10000/' /data/fpd/fpd.conf
+sed -i 's/^\(\s*PollInterval\s*=\s*\).*/\1 200ms/' /data/fpd/fpd.conf
 
 if sed -n '/\[chainpollerconfig\]/,/^\[/p' /data/fpd/fpd.conf | grep -q 'PollSize'; then
   # Update the existing PollSize value
-  sed -i '/\[chainpollerconfig\]/,/\[/ s/^\(\s*PollSize\s*=\s*\).*/\1 100/' /data/fpd/fpd.conf
+  sed -i '/\[chainpollerconfig\]/,/\[/ s/^\(\s*PollSize\s*=\s*\).*/\1 20/' /data/fpd/fpd.conf
 else
   # Append PollSize after BufferSize if PollSize is not present
-  sed -i '/BufferSize = 1000/a PollSize = 100' /data/fpd/fpd.conf
+  sed -i '/BufferSize = 10000/a PollSize = 20' /data/fpd/fpd.conf
 fi
+
+# Add GRPCMaxContentLength and UnsafeResetLastVotedHeight if not present
+if ! grep -q '^GRPCMaxContentLength' /data/fpd/fpd.conf; then
+  cat <<EOF >> /data/fpd/fpd.conf
+[Application Options]
+; The maximum size of the gRPC message in bytes.
+GRPCMaxContentLength = 16777216
+
+; WARNING: If set to 'true', resets the finality provider's last voted height to the calculated start height from poller. WARNING
+UnsafeResetLastVotedHeight = true
+EOF
+else
+  # Update existing GRPCMaxContentLength value to 16777216 and UnsafeResetLastVotedHeight to true
+  sed -i 's/^\(\s*GRPCMaxContentLength\s*=\s*\).*/\1 16777216/' /data/fpd/fpd.conf
+  sed -i 's/^\(\s*UnsafeResetLastVotedHeight\s*=\s*\).*/\1 true/' /data/fpd/fpd.conf
+fi
+
 
 # Word splitting is desired for the command line parameters
 # shellcheck disable=SC2086
