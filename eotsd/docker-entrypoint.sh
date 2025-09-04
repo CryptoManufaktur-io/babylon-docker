f17d608b0373843128a0c045a7871ff3051db3a7
Antonio Savage <antonio.savage@galaxy.com>
2025-09-04 17:23:37 -0400

    update logic in eotsd docker entrypoint

--- eotsd/docker-entrypoint.sh
+++ eotsd/docker-entrypoint.sh
@@ -12,7 +12,7 @@ echo "Initialization complete. Ready to start eotsd."
 
 sed -i '/^\[metrics\]/,/^\[/{ /^[^[]/ s/127\.0\.0\.1/0.0.0.0/g }' /data/eotsd/eotsd.conf
 # Add GRPCMaxContentLength if not present
-if ! grep -q '^GRPCMaxContentLength' /data/eotsd/eotsd.conf; then
+if grep -q '^GRPCMaxContentLength' /data/eotsd/eotsd.conf; then
   # Update existing GRPCMaxContentLength value to 16777216
   sed -i 's/^GRPCMaxContentLength[[:space:]]*=[[:space:]]*.*/GRPCMaxContentLength = 16777216/' /data/eotsd/eotsd.conf
 else
